86b3b8b37893f3562d54081222c06447fb7bde16
unknown <parvinvr@code.edu.az>
2022-12-12 01:59:52 +0400

    new c# work

---
+++ csharp-intronewversion/Program.cs
@@ -0,0 +1,159 @@
+﻿//TASK1
+//getnum()
+//static void getnum()
+// int n
+// if (n%3==1 && n%7==1)
+//	{
+//	global::System.Console.WriteLine("Bolunmur");
+//}
+//else
+//{
+//	global::System.Console.WriteLine("bolunur");
+//TASK2
+//	sum()
+//static void sum()
+//   int n
+
+//	int m
+
+//	sum = 0
+
+//	if (n % 2 == 0 && m % 2 == 0)
+//	{
+//		sum = n + m;
+
+//		result = sum;
+
+//	global::System.Console.WriteLine(result);
+
+
+//	else (n % 2 == 1 && m % 2 == 1)
+
+//	{
+//			break;
+//		}
+//TASK3
+//		getoddNums(13, 28)
+//static void getoddnums(int n, int m)
+//			int sum = 0;
+//			for (int i = n; i < m; i++)
+//			{
+//				if (i % 2 == 1)
+//				{
+//					sum = sum + i;
+//				}
+//			}
+//			//}
+//			result = sum;
+//			Console.WriteLine(result);
+//TASK4
+//getoddnums([]arr)
+//static void getoddNums([] arr)
+//for (int i = 0; i < length; i++)
+//		{
+//			if ([i] % 2 == 1)
+//			{
+//				sum = sum + arr[i];
+//			}
+//		}
+//		result = sum
+//	global::System.Console.WriteLine(result);
+
+
+//TASK5
+//getivnNums([]arr)
+//static void getivnNums([] arr)
+//	int result = 0;
+//for (int i = 0; i < length; i++)
+//{
+//	if ([i] % 2 == 0)
+//	{
+//		sum = sum + arr[i];
+//	}
+//}
+//result = sum
+//	global::System.Console.WriteLine(result);
+
+//TASK6
+//		GetOddNums(int n, int m)
+//static int GetOddNums(int n, int m)
+//	int count = 0;
+//		for (int i = n; i < m; i++)
+//		{
+//			if (i % 2 == 1)
+//			{
+//				count++;
+//			}
+//		}
+//return count;
+//		global::System.Console.WriteLine();
+
+
+//TASK7
+//getoddNums(10, 26)
+//static void getivnnums(int n, int m)
+//			int sum = 0;
+//			for (int i = n; i < m; i++)
+//			{
+//           if (i % 2 == 0)
+//			{
+//			 sum = sum + i;
+//			}
+//			}
+//			result = sum;
+//			Console.WriteLine(result);
+//TASK8
+//getivnnums(14,23)
+//static void getivnnums(int n, int m)
+//    int count = 0;
+//for (int i = n; i < m; i++)
+//{
+//	if (i%2==0)
+//	{
+//		count++;
+//	}
+//}
+//result= int count
+//	global::System.Console.WriteLine(result);
+
+//TASK9
+//getnum()
+//static void getnum()
+//    int n=15
+//    if (n<0)
+//{
+//    global::System.Console.WriteLine("duzgun eded daxil et");
+//}
+//if (n = 1)
+
+//{
+//    global::System.Console.WriteLine("ne sade nede murekkebdir");
+//}
+ 
+//for (int i = 1; i < 15; i++)
+//    {
+//        if (n%i==0)
+//        {
+//            count++;
+//        }
+
+//    }
+
+//if (count>2)
+//{
+//    global::System.Console.WriteLine("murekkebdir");
+//}
+//else
+//{
+//    global::System.Console.WriteLine("sadedir");
+//}
+//TASK 10
+//getfactorial(10)
+//static void getfactorial()
+//    int n;
+//int result = 1;
+//    for (int i = 1; i =< n; i++)
+//{
+//    result == result * i;
+//}
+//global::System.Console.WriteLine(result);
